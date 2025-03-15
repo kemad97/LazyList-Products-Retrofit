9903bc586732daf11f7b9f9c0ba66af62e635ecc
kemad97 <kerolos.emad992@gmail.com>
2025-03-16 00:32:21 +0200

    Update class FetchProductsWorker.kt

--- app/src/main/java/com/example/lazylistproducts/class FetchProductsWorker.kt
+++ app/src/main/java/com/example/lazylistproducts/class FetchProductsWorker.kt
@@ -2,6 +2,7 @@ package com.example.lazylistproducts
 
 import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
@@ -10,24 +11,30 @@ import com.google.gson.Gson
 class FetchProductsWorker(appContext: Context, workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
     override suspend fun doWork(): Result {
         val productDao = ProductDatabase.getDatabase(applicationContext).productDao()
+
+        if (!NetworkUtils.isNetworkAvailable(applicationContext)) {
+            Log.i("FetchProductsWorker", "No internet connection. Using cached data.")
+            val cachedProducts = productDao.getAllProducts()
+            return Result.success(workDataOf("data" to Gson().toJson(cachedProducts)))
+
+        }
+
         return try {
             val response = RetrofitInstance.apiService.getProducts()
             val products = response.products
 
-            if (products.isNotEmpty()) {
-                productDao.insertProducts(products)
-                Log.i("FetchProductsWorker", " Data successfully fetched and stored: ${products.size} products")
-
-                // Return WorkManager success output
-                val data = workDataOf("data" to Gson().toJson(products))
-                return Result.success(data)
-            } else {
-                Log.e("FetchProductsWorker", "API returned empty product list")
-                Result.failure()
+            if (products.isEmpty()) {
+                Log.e("FetchProductsWorker", "API returned empty list")
+                return Result.failure()
             }
+
+            productDao.insertProducts(products)
+            Log.i("FetchProductsWorker", "Data fetched and stored: ${products.size} products")
+
+            Result.success(workDataOf("data" to Gson().toJson(products)))
         } catch (e: Exception) {
             Log.e("FetchProductsWorker", "Error fetching data", e)
-            Result.failure()
+            Result.retry()
         }
     }
 }
